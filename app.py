5d178b85369de7c2040b0002121015e0f32353ae
Bar Hochman <11165655+jochman@users.noreply.github.com>
2019-08-16 21:28:12 +0300

    Benja (#1)
    * idea folder
    
    * idea folder
    
    * add benny
    
    * fix reviewssss
    
    * added postman requests
    
    * added postman requests
    
    * added postman requests
    
    * fix reviewssss
    
    * Delete workspace.xml
    
    * fix payment id
    
    * fixed postman requests
    
    * fixed postman requests

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ auth = "123auth567"
 
 consent_create = False
 current_req_id = "req_id"
+payment_id = 0
 
 headers = {
     "Content-Type": "application/json"
@@ -25,7 +26,8 @@ headers = {
 
 @app.route(api_path + "/v1/consents", methods=["POST"])
 def post():
-    global consent_created, current_req_id
+    global consent_created, current_req_id, payment_id
+    restart_jsons()
     x_req_id = request.headers.get("X-Request-ID")
     accounts = json.loads(request.data).get("access")
     # validating consent
@@ -35,6 +37,7 @@ def post():
             return err_resp
     consent_created = True
     current_req_id = x_req_id
+    payment_id = 0
     return Response(
         json.dumps(
             {
@@ -116,9 +119,7 @@ def get_accounts():
                                 "resourceId": "3dc3d5b3-7023-4848-9853-f5400a64e80f",
                                 "iban": "LV95LATB000222PSD2001",
                                 "currency": "EUR",
-                                "product": "Girokonto",
-                                "cashAccountType": "CurrentAccount",
-                                "name": "Main Account",
+                                "name": "LV95LATB000222PSD2001",
                                 "_links": {
                                     "balances": {"href": "/v1/accounts/3dc3d5b3-7023-4848-9853-f5400a64e80f/balances"},
                                     "transactions": {
@@ -128,9 +129,7 @@ def get_accounts():
                                 "resourceId": "3dc3d5b3-7023-4848-9853-f5400a64e81g",
                                 "iban": "LV68LATB000222PSD2002",
                                 "currency": "EUR",
-                                "product": "Fremdwährungskonto",
-                                "cashAccountType": "CurrentAccount",
-                                "name": "US Dollar Account",
+                                "name": "LV68LATB000222PSD2002",
                                 "_links": {
                                     "balances": {"href": "/v1/accounts/3dc3d5b3-7023-4848-9853-f5400a64e81g/balances"},
                                     "transactions": {
@@ -143,9 +142,206 @@ def get_accounts():
         )
     return err_resp
 
-# TODO accounts
-# TODO sepa-credit-payments
-# TODO funds-confirmation
+
+@app.route(api_path + "/v1/accounts/<account>", methods=["GET"])
+def get_account(account):
+    header = request.headers
+    if header.get("X-Request-ID") == current_req_id and header.get("Consent-ID") == consent:
+        if account == "LV95LATB000222PSD2001":
+            headers["X-Request-ID"] = current_req_id
+            return Response(
+                json.dumps(
+                    {
+                        "accounts":
+                            [
+                                {
+                                    "resourceId": "3dc3d5b3-7023-4848-9853-f5400a64e80f",
+                                    "iban": "LV95LATB000222PSD2001",
+                                    "currency": "EUR",
+                                    "product": "Girokonto",
+                                    "cashAccountType": "CurrentAccount",
+                                    "name": "Main Account",
+                                    "_links": {
+                                        "balances": {
+                                            "href": "/v1/accounts/3dc3d5b3-7023-4848-9853-f5400a64e80f/balances"},
+                                        "transactions": {
+                                            "href": "/v1/accounts/3dc3d5b3-7023-4848-9853-f5400a64e80f/transactions"}}
+                                }
+                            ]
+                    }),
+                status=200,
+                headers=headers
+            )
+        elif account == "LV68LATB000222PSD2002":
+            headers["X-Request-ID"] = current_req_id
+            return Response(
+                json.dumps(
+                    {
+                        "accounts":
+                            [
+                                {
+                                    "resourceId": "3dc3d5b3-7023-4848-9853-f5400a64e81g",
+                                    "iban": "LV68LATB000222PSD2002",
+                                    "currency": "EUR",
+                                    "product": "Fremdwährungskonto",
+                                    "cashAccountType": "CurrentAccount",
+                                    "name": "US Dollar Account",
+                                    "_links": {
+                                        "balances": {
+                                            "href": "/v1/accounts/3dc3d5b3-7023-4848-9853-f5400a64e81g/balances"},
+                                        "transactions": {
+                                            "href": "/v1/accounts/3dc3d5b3-7023-4848-9853-f5400a64e81g/transactions"}}
+                                },
+                            ]
+                    }),
+                status=200,
+                headers=headers
+            )
+        else:
+            return Response(
+                json.dumps(
+                    "No account with such iban was found"
+                ),
+                status=400
+            )
+
+    return err_resp
+
+
+@app.route(api_path + "/v1/funds-confirmations", methods=["POST"])
+def get_funds_confirmations():
+    header = request.headers
+    if header.get("X-Request-ID") == current_req_id and header.get("Consent-ID") == consent:
+        headers["X-Request-ID"] = current_req_id
+        headers["Content-Type"] = "application/json"
+        account = json.loads(request.data).get("account")
+        req_iban = account["iban"]
+        instructed_amount = json.loads(request.data).get("instructedAmount")
+        req_amount = instructed_amount["amount"]
+        req_currency = instructed_amount["currency"]
+        with open("./res/balances.json") as f:
+            balances = json.load(f)
+            if req_iban not in balances.keys():
+                return err_resp
+            cur_amount = balances[req_iban][req_currency]
+        if req_amount > cur_amount:
+            headers["X-Request-ID"] = current_req_id
+            return Response(
+                json.dumps(
+                    {
+                        "fundsAvailable": False
+                    }),
+                status=200,
+                headers=headers
+            )
+        else:
+            headers["X-Request-ID"] = current_req_id
+            return Response(
+                json.dumps(
+                    {
+                        "fundsAvailable": True
+                    }),
+                status=200,
+                headers=headers
+            )
+    return err_resp
+
+
+@app.route(api_path + "/v1/payments/sepa-credit-transfers", methods=["POST"])
+def transfer_sepa_credit():
+    header = request.headers
+    if header.get("X-Request-ID") == current_req_id and header.get("Consent-ID") == consent \
+            and header.get("TPP-Redirect-URI") == "site.com" and header.get("Content-Type") == "application/json":
+        tpp_redirect_uri = header.get("TPP-Redirect-URI")
+        instructed_amount = json.loads(request.data).get("instructedAmount")
+        req_amount = instructed_amount["amount"]
+        req_currency = instructed_amount["currency"]
+        debtor_account = json.loads(request.data).get("debtorAccount")
+        debtor_iban = debtor_account["iban"]
+        creditor_name = json.loads(request.data).get("creditorName")
+        creditor_account = json.loads(request.data).get("creditorAccount")
+        creditor_iban = creditor_account["iban"]
+        with open("./res/balances.json", "r+") as f1:
+            balances = json.load(f1)
+            if debtor_iban not in balances.keys() or creditor_iban not in balances.keys() \
+                    or req_currency not in balances[debtor_iban]:
+                return err_resp
+            global payment_id
+            last_payment = payment_id
+            payment_id += 1
+            cur_amount_debtor = balances[debtor_iban][req_currency]
+            if req_amount > cur_amount_debtor:
+                headers["X-Request-ID"] = current_req_id
+                headers["TPP-Redirect-URI"] = tpp_redirect_uri
+                headers["ASPSP-SCA-Approach"] = "REDIRECT"
+                temp_payment_id = last_payment
+                return Response(
+                    json.dumps(
+                        {
+                            "paymentId": temp_payment_id,
+                            "_links": {
+                                "scaRedirect": tpp_redirect_uri,
+                                "self": "/v1/payments/" + str(temp_payment_id),
+                                "status": "Rejected",
+                                "scaStatus": "Accepted"
+                            }
+                        }),
+                    status=200,
+                    headers=headers
+                )
+            else:
+                temp_payment_id = last_payment
+                headers["X-Request-ID"] = current_req_id
+                headers["TPP-Redirect-URI"] = tpp_redirect_uri
+                headers["ASPSP-SCA-Approach"] = "REDIRECT"
+                balances[debtor_iban][req_currency] = cur_amount_debtor - req_amount
+                if req_currency not in balances[creditor_iban]:
+                    balances[creditor_iban][req_currency] = req_amount
+                else:
+                    balances[creditor_iban][req_currency] = balances[creditor_iban][req_currency] + req_amount
+                with open("./res/payments.json", "r+") as f2:
+                    payments = json.load(f2)
+                    payments[temp_payment_id] = {
+                        "creditor_iban": creditor_iban,
+                        "debtor_iban": debtor_iban,
+                        "currency": req_currency,
+                        "amount": req_amount
+                    }
+                    f2.seek(0)
+                    json.dump(payments, f2, indent=4)
+                    f2.truncate()
+                    f1.seek(0)
+                    json.dump(balances, f1, indent=4)
+                return Response(
+                    json.dumps(
+                        {
+                            "paymentId": temp_payment_id,
+                            "_links": {
+                                "scaRedirect": tpp_redirect_uri,
+                                "self": "/v1/payments/" + str(temp_payment_id),
+                                "status": "Accepted",
+                                "scaStatus": "Accepted"
+                            }
+                        }),
+                    status=200,
+                    headers=headers
+                )
+    return err_resp
+
+
+def restart_jsons():
+    """ Getting json back to default
+    """
+    with open("./res/payments.json", "w+") as f2:
+        data = {}
+        json.dump(data, f2, indent=4)
+
+    with open("./res/balances.json", "w+") as f2:
+        data = {
+            "LV95LATB000222PSD2001": {"EUR": 500},
+            "LV68LATB000222PSD2003": {"EUR": 200}
+        }
+        json.dump(data, f2, indent=4)
 
 
 if __name__ == '__main__':
